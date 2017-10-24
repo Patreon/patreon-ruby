60a342eba17f91f6e0428faad0ffdbc81b995991
David Kettler <21echoes@gmail.com>
2017-10-24 14:04:22 -0700

    allow clients to request unparsed json

--- lib/patreon/api.rb
+++ lib/patreon/api.rb
@@ -9,34 +9,59 @@ module Patreon
       @access_token = access_token
     end
 
-    def fetch_user(includes=nil, fields=nil)
-      get_json(Utils::JSONAPI::URLUtil.build_url('current_user',includes,fields))
+    def fetch_user(includes=nil, fields=nil, parse=True)
+      get_parsed_or_unparsed(
+          Utils::JSONAPI::URLUtil.build_url('current_user',includes,fields),
+          parse
+      )
     end
 
-    def fetch_campaign(includes=nil, fields=nil)
-      get_json(Utils::JSONAPI::URLUtil.build_url('current_user/campaigns',includes,fields))
+    def fetch_campaign(includes=nil, fields=nil, parse=True)
+      get_parsed_or_unparsed(
+          Utils::JSONAPI::URLUtil.build_url('current_user/campaigns',includes,fields),
+          parse
+      )
     end
 
-    def fetch_campaign_and_patrons(includes=nil, fields=nil)
+    def fetch_campaign_and_patrons(includes=nil, fields=nil, parse=True)
       includes ||= Schemas::Campaign.default_relationships + [Schemas::Campaign::Relationships::PLEDGES]
-      fetch_campaign(includes, fields)
+      fetch_campaign(includes, fields, parse=parse)
     end
 
-    def fetch_page_of_pledges(campaign_id, page_size, cursor=nil, includes=nil, fields=nil)
+    def fetch_page_of_pledges(campaign_id, page_size, cursor=nil, includes=nil, fields=nil, parse=True)
       url = "campaigns/#{campaign_id}/pledges"
       url += "?page%5Bcount%5D=#{CGI::escape(page_size.to_s)}"
       url += "&page%5Bcursor%5D=#{CGI::escape(cursor.to_s)}" if cursor
-      get_json(Patreon::Utils::JSONAPI::URLUtil.build_url(url,includes,fields))
+      get_parsed_or_unparsed(
+          Patreon::Utils::JSONAPI::URLUtil.build_url(url,includes,fields),
+          parse
+      )
     end
 
     private
 
+    def get_parsed_or_unparsed(suffix, parse)
+      if parse
+          return get_jsonapi_doc(suffix)
+      return get_json(suffix)
+    end
+
+    def get_jsonapi_doc(suffix)
+      response = get_data(suffix)
+      return JSON::Api::Vanilla.parse(response.body)
+    end
+
     def get_json(suffix)
+      response = get_data(suffix)
+      return JSON.parse(res.body)
+    end
+
+    def get_data(suffix)
       url = URI.parse("https://api.patreon.com/oauth2/api/#{suffix}")
       req = Net::HTTP::Get.new(url.to_s)
       req['Authorization'] = "Bearer #{@access_token}"
-      res = Net::HTTP.start(url.host, url.port, :use_ssl => true) {|http| http.request(req)}
-      return JSON::Api::Vanilla.parse(res.body)
+      response = Net::HTTP.start(url.host, url.port, :use_ssl => true) {|http| http.request(req)}
+      return response
     end
   end
 end
